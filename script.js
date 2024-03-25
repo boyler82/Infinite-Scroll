4f72309fa1e22405807ad10ea7f0ad453edcfe2d
boyler82 <tomasz26bojko@gmail.com>
2024-03-25 12:45:33 +0100

    scrip.js fille added

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let photosArray = []
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
 
 // Unsplash API
-const count = 1;
+const count = 10;
 const apiKey = "bMUG-xmnXs5DBxSVG6MYdHFuURy3zoinhjhQSE94RgY";
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// check if image were loaded
+function imageLoaded() {
+    console.log("loaded img");
+    imagesLoaded ++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        console.log('ready=', ready);
+    }
+
+}
+
+// crating images
 function displayPhotos() {
+    totalImages =photosArray.length;
+    console.log('total images=', totalImages);
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
         item.setAttribute('href', photo.links.html);
@@ -18,6 +35,8 @@ function displayPhotos() {
         img.setAttribute('src', photo.urls.regular);
         img.setAttribute('alt', photo.alt_description);
         img.setAttribute('title', photo.alt_description);
+        // Event listner check if finish loading
+        img.addEventListener('load', imageLoaded);
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
@@ -33,11 +52,17 @@ async function getPhotos() {
 // fjhgjh
     }
 }
-// // Check if near botom of page , load new photow
-// window.addEventListener('scroll', () => {
-//     console.log("scerollinggggggg"); 
+
+// Event listner LOAD
+
+
+// Check if near botom of page , load new photow
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1500) {
+        console.log("scroling")
+    }
         
-//     });
+    });
 
 
 // start
